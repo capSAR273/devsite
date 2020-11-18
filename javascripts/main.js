1017521651ccc94c1f0ee041916505a762d84f16
capSAR273 <capsar273@gmail.com>
2020-11-18 09:15:26 -0500

    Updating sidebar height

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
 console.log('This would be the main JS file.');
+
+var contentHeight = document.getElementById('main_content_wrap').style.height;
+
+document.getElementsByClassName("sidebar").style.height = contentHeight;
