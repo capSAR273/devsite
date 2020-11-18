6257ca01d9bc43c551e8947d26c2e447c58b97ed
capSAR273 <capsar273@gmail.com>
2020-11-18 10:15:43 -0500

    Double sidebar

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,4 @@
-console.log('This would be the main JS file.');
-(function(window,document,undefined){
-    window.onload = setSidebarHeight;
-    function setSidebarHeight(){
-        var contentHeight = document.getElementById('main_content_wrap').style.height;
-        document.getElementsByClassName("sidebar").style.height = contentHeight;
-    }
-})(window,document,undefined);
+console.log('Main JS file loaded.');
+
 
 
