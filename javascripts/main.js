40ae8d10fb5b1d8db4bd8e84ea1c96610e687ab1
capSAR273 <capsar273@gmail.com>
2020-11-18 09:28:45 -0500

    Tweaking sidebar

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,10 @@
 console.log('This would be the main JS file.');
+(function(window,document,undefined){
+    window.onload = setSidebarHeight;
+    function setSidebarHeight(){
+        var contentHeight = document.getElementById('main_content_wrap').style.height;
+        document.getElementsByClassName("sidebar").style.height = contentHeight;
+    }
+})(window,document,undefined);
 
-var contentHeight = document.getElementById('main_content_wrap').style.height;
 
-document.getElementsByClassName("sidebar").style.height = contentHeight;
